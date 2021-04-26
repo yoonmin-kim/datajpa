ef102652f6308292a28723d725b64400b0ce59a2
YoonMin <rladbsals23@gmail.com>
2021-04-26 21:43:14 +0900

    feat : 쿼리 메소드 기능 3
    [반환타입]
    find...By...형식을 지키면
    컬렉션, 단건, 옵셔널<단건> 원하는 반환타입으로
    조회가 가능하다.
    
    [순수 JPA페이징과 정렬]
    페이징 : setFirstResult(), setMaxResuls() 를
    사용하여 limit, offset설정 가능
    
    totalCount : 예시) select count(m) from Member m

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.member.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -139,5 +140,22 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> findList = memberRepository.findListByUsername("AAA");
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> findOptional = memberRepository.findOptionalByUsername("AAA");
+
+        System.out.println("findList = " + findList);
+        System.out.println("findMember = " + findMember);
+        System.out.println("findOptional = " + findOptional);
+
     }
 }
