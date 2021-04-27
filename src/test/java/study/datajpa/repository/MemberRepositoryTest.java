3c3bdb0fbcbc361435c2f5caf5410643c6404e86
YoonMin <rladbsals23@gmail.com>
2021-04-27 21:55:04 +0900

    feat : 쿼리 메소드 기능 6
    [Entity Graph]
    JPA의 N+1 문제를 해결하기 위해서 일반적으로 fetch조인을 사용한다.
    스프링데이터 jpa는 fetch을 간단한 어노테이션으로 적용하능하다.
    
    ex)
    @EntityGraph(attributePaths = {"team"})
    List<Member> findMEmberEntityGraph();

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -220,4 +220,29 @@ class MemberRepositoryTest {
         //then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = memberRepository.findMemberFetchJoin();
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+    }
 }
