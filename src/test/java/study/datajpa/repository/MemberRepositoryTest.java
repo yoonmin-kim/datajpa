6d7b3ec1941905a4394695d6fdf3c934af087b1b
YoonMin <rladbsals23@gmail.com>
2021-04-27 22:18:11 +0900

    feat : 쿼리 메소드 기능 7
    [Hint]
    단순 조회용도로 엔티티를 사용하고 싶을때 엔티티를 조회한 뒤
    em.clear()를 하는게 일반적이지만 스프링 데이터 JPA에서는 어노테이션으로
    기능을 제공한다
    ex)
    @QueryHints(value = @QueryHint(name="org.hibernate.readOnly", value = "true"))
    Member findReadOnlyByUsername(String username);
    
    [lock]
    select for update 와 같은 락을 사용하기 위한 어노테이션도 제공한다.
    ex)
    @Lock(LockModeType.PESSIMISTIC_WRITE)
    List<Member> findLockByUsername(String username);

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -245,4 +245,31 @@ class MemberRepositoryTest {
             System.out.println("member.team = " + member.getTeam().getName());
         }
     }
+
+    @Test
+    public void queryHint() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1, 10"));
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1, 10"));
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+    }
+
+
 }
